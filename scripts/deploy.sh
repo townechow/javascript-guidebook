38d8fcb70db88764ef327b1c453fda4d3ee6aa4b
zhouzhongyang <zhouzhongyang@sandboxol.com>
2023-07-05 11:29:50 +0800

    chore: deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,13 +11,13 @@ rm -rf $TEMP_PATH
 mkdir $TEMP_PATH
 cd $TEMP_PATH
 git init
-git pull https://github.com/tsejx/javascript-guidebook.git gh-pages
+git pull https://github.com/townechow/javascript-guidebook.git gh-pages
 cp -r ../../dist/* .
 
 # commit and push changes
 git add -A
 git commit --am -m "build: deploy documentation"
-git push -f https://github.com/tsejx/javascript-guidebook.git master:gh-pages
+git push -f https://github.com/townechow/javascript-guidebook.git master:gh-pages
 
 ## clean
 cd
